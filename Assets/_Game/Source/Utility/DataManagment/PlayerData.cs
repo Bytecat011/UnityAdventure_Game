0e380e3fc45bdef890a52c250d221436dffee204
Igor Fomin <ifomin@khagheriantar.am>
2025-06-23 20:14:13 +0400

    Implement player data

---
+++ Assets/_Game/Source/Utility/DataManagment/PlayerData.cs
@@ -0,0 +1,10 @@
+using Game.Meta.Features.Resources;
+using System.Collections.Generic;
+
+namespace Game.Utility.DataManagment
+{
+    public class PlayerData
+    {
+        public Dictionary<ResourceType, int> ResourceData;
+    }
+}
